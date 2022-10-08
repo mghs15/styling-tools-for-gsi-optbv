4628a48ab5991786f2c19d772f7d330e5091b2c9
mghs15 <40787295+mghs15@users.noreply.github.com>
2022-10-08 12:12:02 +0900

    Add files via upload

---
+++ convert.js
@@ -0,0 +1,528 @@
+const fs = require('fs');
+
+const style = require('./template.json');
+
+const layers = style.layers;
+
+const tmp = {
+  "r": {}, "g": {}, "b": {}, "m": {}
+};
+const tmp2 = {
+  "r": {}, "g": {}, "b": {}, "m": {}
+};
+
+const collectColorInfo = (colorInfo)=>{
+  if(Array.isArray(colorInfo)){
+    colorInfo.forEach( colorInfoElement => {
+      collectColorInfo(colorInfoElement);
+    });
+  }else if(typeof(colorInfo)=="string" && ( colorInfo.match(/^rgb/) || colorInfo.match(/^hsl/))){
+    
+    const ct = classifyColor(colorInfo);
+    
+    if(tmp[ct][colorInfo]){
+      tmp[ct][colorInfo] += 1;
+    }else{
+      tmp[ct][colorInfo] = 1;
+    }
+    return colorInfo;
+  }
+}
+
+const classifyColor = (colorInfo) => {
+  //colorInfoはString
+
+  const colStr = parsecolor(colorInfo);
+  const cr = colStr[1];
+  const cg = colStr[2];
+  const cb = colStr[3];
+  
+  const ct = (cb == cr && cb == cg) ? "m" : (cr >= cg && cr >= cb) ? "r" : (cg >= cr && cg >= cb) ? "g" : "b";
+  
+  return ct;
+}
+
+
+/*************************************************/
+/*RGB→HSLの変換関係設定                         */
+/*************************************************/
+var calcsl = function(max, min) {
+    let L = (max + min)/(2*255);
+    let total = max + min;
+    let S=(max - min)/255;
+    if((total <= 0) || (total >= 510)){
+        S = 0;
+    }else if(total < 255){
+        S = (max-min)/(total);
+    }else{
+        S = (max-min)/(255*2-total);
+    }
+    let SL = {"s" : S, "l" : L};
+    return SL;
+}
+
+var rgb2hsl = function(r, g, b, a = 1) {
+    let max=0; let middle=0; let min = 0; 
+    let h=0; let s=0; let l=0; 
+    if((r == g) && (r == b)){
+        max = r; middle=b; min = g; 
+        h = 0;
+        let sl = calcsl(max, min);
+        s = sl.s;
+        l = sl.l;
+    }else if((r <= g) && (r < b)){
+        min = r;
+        if(g < b){
+            middle=g; max = b; 
+        }else{ 
+            middle=b; max = g; 
+        }
+        h = 60*((b-g)/(max-min))+180;
+        let sl = calcsl(max, min);
+        s = sl.s;
+        l = sl.l;
+    }else if((g <= b) && (g < r)){
+        min = g; 
+        if(r < b){
+            middle=r; max = b; 
+        }else{ 
+            middle=b; max = r; 
+        } 
+        h = 60*((r-b)/(max-min))+300;
+        let sl = calcsl(max, min);
+        s = sl.s;
+        l = sl.l;
+    }else{
+        min = b; 
+        if(g < r){
+            middle=g; max = r; 
+        }else{ 
+            middle=r; max = g; 
+        }
+        h = 60*((g-r)/(max-min))+60;
+        let sl = calcsl(max, min);
+        s = sl.s;
+        l = sl.l;
+    }
+    
+    if(h > 360){
+        h = h - 360;
+    }else if(h < 0){
+        h = h + 360;
+    }
+    
+    const hsl = [ Math.floor(h), Math.floor(s*100), Math.floor(l*100), a ];
+    return hsl;
+}
+
+/*************************************************/
+/*HSL→RGBの変換関係設定                         */
+/*************************************************/
+const hsl2rgb = function(h, s, l, a = 1) {
+
+    const max = l + (s * (1 - Math.abs((2 * l) - 1)) / 2);
+    const min = l - (s * (1 - Math.abs((2 * l) - 1)) / 2);
+
+    let rgb;
+    const i = parseInt(h / 60);
+
+    switch (i) {
+      case 0:
+      case 6:
+        rgb = [max, min + (max - min) * (h / 60), min];
+        break;
+      case 1:
+        rgb = [min + (max - min) * ((120 - h) / 60), max, min];
+        break;
+      case 2:
+        rgb = [min, max, min + (max - min) * ((h - 120) / 60)];
+        break;
+      case 3:
+        rgb = [min, min + (max - min) * ((240 - h) / 60), max];
+        break;
+      case 4:
+        rgb = [min + (max - min) * ((h - 240) / 60), min, max];
+        break;
+      case 5:
+        rgb = [max, min, min + (max - min) * ((360 - h) / 60)];
+        break;
+    }
+    
+    return [Math.round(rgb[0] * 255), Math.round(rgb[1] * 255), Math.round(rgb[2] * 255), a] ;
+    
+  }
+
+
+/*************************************************/
+/*"rgba(r,g,b,a)"などのパース                    */
+/*************************************************/
+var parsecolor = function(txt){
+  if( (txt.indexOf("rgba") == 0) || (txt.indexOf("hsla") == 0) ){
+    var length = txt.length - 1;
+    var type= txt.slice(0, 4);
+    txt = txt.slice(5,length );
+    var col = txt.split(",");
+  }else{
+    var length = txt.length - 1;
+    var type= txt.slice(0, 3) + "a";
+    txt = txt.slice(4,length );
+    var col = txt.split(",");
+    col.push(1);
+  }
+  const color = [];
+  color.push( type );
+  color.push( parseInt(col[0]) );
+  color.push( parseInt(col[1]) );
+  color.push( parseInt(col[2]) );
+  color.push( Number(col[3]) );
+  return color;
+}
+
+const addArrEle = (arr, ele) => {
+  const n = arr.length;
+  if(Array.isArray(arr[n-1])){
+    addArrEle(arr[n-1],ele);
+  }else{
+    arr.push(ele);
+  }
+}
+
+const convertColor = (colorInfo, arr=[], info={}) => {
+  //colorInfoが配列か色情報文字列（例：'-background-base-main-'）かどうかで分岐
+  if(Array.isArray(colorInfo)){
+    colorInfo.forEach( colorInfoElement => {
+      arr.push(convertColor(colorInfoElement, [], info));
+    });
+    return arr;
+  }else if(typeof(colorInfo)=="string" && ( colorInfo.match(/^rgb/) || colorInfo.match(/^hsl/) || colorInfo.match(/^-/))){
+    
+    //修正
+    let colArr1;
+    if(colorInfo.match(/^rgb/) || colorInfo.match(/^hsl/)){
+      //テキスト形式を配列へ
+      colArr1 = parsecolor(colorInfo);
+    }else{
+      //console.log(colorInfo);
+      info.colorInfo = colorInfo;
+      
+      const arrColorOpacity = colorInfo.split("#");
+      const colorKey = arrColorOpacity[0];
+      const opacity = arrColorOpacity[1];
+      
+      const colSetArr = colorSet[colorKey] || colorSet["default"] || [255, 255, 255];
+      colArr1 = ["rgba", ...colSetArr, opacity];
+    }
+    
+    colArr = changeColor(colArr1, info);
+    
+    //色の変更
+    const colTxt = `${colArr[0]}(${colArr[1]},${colArr[2]},${colArr[3]},${colArr[4]})`;
+    
+    const ct = classifyColor(colTxt);
+    
+    if(tmp2[ct][colTxt]){
+      tmp2[ct][colTxt] += 1;
+    }else{
+      tmp2[ct][colTxt] = 1;
+    }
+    
+    return(colTxt);
+    
+  }else{
+    return(colorInfo);
+  }
+  
+}
+
+const colorSets = {
+  
+  "basic" : {
+    'default': [ 255, 255, 240 ],
+    
+    '-background-base-main-': [ 255, 255, 240 ],
+    
+    '-railway-normal-main-': [ 100, 100, 100 ],
+    '-railway-shinkansen-main-': [ 0, 0, 255 ],
+    '-railway-all-back-': [ 255, 255, 240 ],
+    //'-railway-normal-blank-': [255, 255, 255], デフォルト色でよい
+    '-railway-subway-main-': [ 0, 0, 255 ],
+    
+    '-road-major-main-': [ 240, 200, 120 ],
+    '-road-normal-main-': [ 240, 240, 240 ],
+    '-road-highway-main-': [ 255, 190, 190 ],
+    '-road-prefectural-main-': [ 255, 210, 110 ],
+    '-road-expressway-main-': [ 0, 150, 0 ],
+    '-road-all-back-': [ 220, 220, 220 ],
+    '-road-edge-': [ 230, 230, 230],
+    
+    '-building-normal-main-': [ 230, 230, 230 ],
+    '-building-middle-main-': [ 220, 220, 220 ],
+    '-building-high-main-': [ 210, 210, 210 ],
+    '-building-normal-outline-': [ 200, 200, 200],
+    
+    '-wetland-main-main-': [ 235, 242, 235 ],
+    '-landform-main-main-': [ 230, 230, 170 ],
+    
+    '-water-main-main-': [ 190, 210, 255 ],
+    '-water-main-vivid-': [ 20, 90, 255],
+    '-water-main-blank-': [ 255, 255, 255 ],
+    
+    '-border-muni-main-': [ 200, 145, 255 ],
+    
+    '-text-gray-main-': [ 100, 100, 100 ],
+    '-text-black-main-': [ 50, 50, 50 ],
+    '-text-green-main-': [ 20, 100, 70 ],
+    '-text-blue-main-': [ 0, 0, 150 ],
+    '-text-white-main-': [ 255, 255, 255 ],
+    '-text-white-halo-': [ 255, 255, 255 ],
+    
+    '-line-gray-': [ 120, 120, 120 ],
+  },
+  
+  "dark": {
+    'default': [ 30, 30, 30 ],
+    
+    '-background-base-main-': [ 30, 30, 30 ],
+    
+    '-railway-normal-main-': [ 180, 180, 180 ],
+    '-railway-shinkansen-main-': [ 255, 255, 255 ],
+    '-railway-all-back-': [ 30, 30, 30 ],
+    '-railway-subway-main-': [ 180, 180, 180 ],
+    
+    '-road-major-main-': [ 60, 60, 60 ],
+    '-road-normal-main-': [ 40, 40, 40 ],
+    '-road-highway-main-': [ 80, 80, 80 ],
+    '-road-prefectural-main-': [ 60, 60, 60 ],
+    '-road-expressway-main-': [ 150, 150, 150 ],
+    '-road-all-back-': [ 30, 30, 30 ],
+    '-road-edge-': [ 35, 35, 35 ],
+    
+    '-building-normal-main-': [ 20, 20, 20 ],
+    '-building-middle-main-': [ 40, 40, 40 ],
+    '-building-high-main-': [ 100, 100, 100 ],
+    '-building-normal-outline-': [ 10, 10, 10 ],
+    
+    '-wetland-main-main-': [ 60, 60, 60 ],
+    '-landform-main-main-': [ 50, 50, 50 ],
+    
+    '-water-main-main-': [ 0, 0, 0 ],
+    '-water-main-vivid-': [ 80, 80, 80 ],
+    '-water-main-blank-': [ 0, 0, 0 ],
+    
+    '-border-muni-main-': [ 180, 180, 180 ],
+    
+    '-text-gray-main-': [ 150, 150, 150 ],
+    '-text-black-main-': [ 200, 200, 200 ],
+    '-text-green-main-': [ 230, 230, 230 ],
+    '-text-blue-main-': [ 200, 200, 200 ],
+    '-text-white-main-': [ 255, 255, 255 ],
+    '-text-white-halo-': [ 50, 50, 50 ],
+    
+    '-line-gray-': [ 80, 80, 80 ],
+  },
+  
+  "dark2": {
+    'default': [ 30, 30, 30 ],
+    
+    '-background-base-main-': [ 30, 30, 30 ],
+    
+    '-railway-normal-main-': [ 79, 226, 255 ],
+    '-railway-shinkansen-main-': [ 255, 255, 255 ],
+    '-railway-all-back-': [ 30, 30, 30 ],
+    '-railway-subway-main-': [ 161, 218, 230 ],
+    
+    '-road-major-main-': [ 60, 60, 60 ],
+    '-road-normal-main-': [ 40, 40, 40 ],
+    '-road-highway-main-': [ 80, 90, 100 ],
+    '-road-prefectural-main-': [ 60, 60, 60 ],
+    '-road-expressway-main-': [ 100, 110, 120 ],
+    '-road-all-back-': [ 30, 30, 30 ],
+    '-road-edge-': [ 35, 35, 35 ],
+    
+    '-building-normal-main-': [ 20, 25, 30 ],
+    '-building-middle-main-': [ 40, 45, 50 ],
+    '-building-high-main-': [ 50, 55, 80 ],
+    '-building-normal-outline-': [ 20, 20, 30 ],
+    
+    '-wetland-main-main-': [ 100, 100, 100 ],
+    '-landform-main-main-': [ 50, 50, 50 ],
+    
+    '-water-main-main-': [ 0, 0, 0 ],
+    '-water-main-vivid-': [ 80, 80, 80 ],
+    '-water-main-blank-': [ 0, 0, 0 ],
+    
+    '-border-muni-main-': [ 180, 180, 180 ],
+    
+    '-text-gray-main-': [ 150, 150, 150 ],
+    '-text-black-main-': [ 200, 200, 200 ],
+    '-text-green-main-': [ 200, 230, 255 ],
+    '-text-blue-main-': [ 200, 200, 255 ],
+    '-text-white-main-': [ 255, 255, 255 ],
+    '-text-white-halo-': [ 50, 50, 50 ],
+    
+    '-line-gray-': [ 80, 90, 100 ],
+  },
+  
+  "blueprint": {
+    'default': [ 0, 160, 255 ],
+    
+    '-background-base-main-': [ 0, 160, 255 ],
+    
+    '-railway-normal-main-': [ 255, 255, 255 ],
+    '-railway-shinkansen-main-': [ 255, 255, 255 ],
+    '-railway-all-back-': [ 0, 160, 255 ],
+    '-railway-subway-main-': [ 255, 255, 255 ],
+    
+    '-road-major-main-': [ 0, 160, 255 ],
+    '-road-normal-main-': [ 0, 160, 255 ],
+    '-road-highway-main-': [ 0, 160, 255  ],
+    '-road-prefectural-main-': [ 0, 160, 255 ],
+    '-road-expressway-main-': [ 51, 180, 255 ],
+    '-road-all-back-': [ 255, 255, 255 ],
+    '-road-edge-': [ 255, 255, 255 ],
+    
+    '-building-normal-main-': [ 255, 255, 255 ],
+    '-building-middle-main-': [ 255, 255, 255 ],
+    '-building-high-main-': [ 255, 255, 255 ],
+    
+    '-wetland-main-main-': [ 255, 255, 255 ],
+    '-landform-main-main-': [ 255, 255, 255 ],
+    
+    '-water-main-main-': [ 210, 240, 255 ],
+    '-water-main-vivid-': [ 255, 255, 255 ],
+    '-water-main-blank-': [ 255, 255, 255 ],
+    
+    '-border-muni-main-': [ 255, 255, 255 ],
+    
+    '-text-gray-main-': [ 255, 255, 255 ],
+    '-text-black-main-': [ 255, 255, 255 ],
+    '-text-green-main-': [ 255, 255, 255 ],
+    '-text-blue-main-': [ 255, 255, 255 ],
+    '-text-white-main-': [ 255, 255, 255 ],
+    '-text-white-halo-': [ 0, 160, 255 ],
+    
+    '-line-gray-': [ 255, 255, 255 ],
+  },
+  
+  "radar": {
+    'default': [ 0, 0, 0 ],
+    
+    '-background-base-main-': [ 0, 0, 0 ],
+    
+    '-railway-normal-main-': [ 0, 255, 100 ],
+    '-railway-shinkansen-main-': [ 0, 255, 100 ],
+    '-railway-all-back-': [ 0, 0, 0 ],
+    '-railway-subway-main-': [ 0, 255, 100 ],
+    
+    '-road-major-main-': [ 0, 0, 0 ],
+    '-road-normal-main-': [ 0, 0, 0 ],
+    '-road-highway-main-': [ 0, 0, 0 ],
+    '-road-prefectural-main-': [ 0, 0, 0 ],
+    '-road-expressway-main-': [ 0, 0, 0 ],
+    '-road-all-back-': [ 0, 255, 100  ],
+    '-road-edge-': [ 0, 255, 100  ],
+    
+    '-building-normal-main-': [ 0, 255, 100 ],
+    '-building-middle-main-': [ 0, 255, 100 ],
+    '-building-high-main-': [ 0, 255, 100 ],
+    
+    '-wetland-main-main-': [ 0, 255, 100 ],
+    '-landform-main-main-': [ 0, 255, 100 ],
+    
+    '-water-main-main-': [ 30, 30, 30 ],
+    '-water-main-vivid-': [ 0, 255, 100 ],
+    '-water-main-blank-': [ 0, 255, 100 ],
+    
+    '-border-muni-main-': [ 0, 255, 100 ],
+    
+    '-text-gray-main-': [ 0, 255, 100 ],
+    '-text-black-main-': [ 0, 255, 100 ],
+    '-text-green-main-': [ 0, 255, 100 ],
+    '-text-blue-main-': [ 0, 255, 100 ],
+    '-text-white-main-': [ 0, 255, 100 ],
+    '-text-white-halo-': [ 0, 0, 0 ],
+    
+    '-line-gray-': [ 0, 255, 100 ],
+  }
+  
+}
+
+const mode = "dark2";
+colorSet = colorSets[mode] || colorSets["basic"];
+
+const changeColor = (arr, info={}) => {
+  
+  const hsla = rgb2hsl(arr[1], arr[2], arr[3], arr[4]);
+  
+  let h = hsla[0];
+  let s = hsla[1];
+  let l = hsla[2];
+  let a = hsla[3]
+  
+  
+  if(mode == "m"){
+  //モノクロ
+    if( s > 0 ) s = 1;
+    if( l < 50 && l > 0 && !info["prop-name"].match("text-color")) l = l + (50 - l);
+    return(["hsla", h, s + "%", l + "%", a]);
+  
+  }else if(mode == "m2"){
+  //モノクロ風（少し色付き）
+    h = 200;
+    if( s > 0 ) s = 10;
+    if( s > 0 && info["prop-name"].match("text-color") ) s = 30;
+    if( l < 50 && l > 0 && !info["prop-name"].match("text-color")) l = l + (50 - l);
+    return(["hsla", h, s + "%", l + "%", a]);
+  
+  }else if(mode == "m3"){
+  //モノクロ（水域の色調整）
+    if( s > 0 ) s = 1;
+    if( l < 50 && l > 0 && !info["prop-name"].match("text-color")) l = l + (50 - l);
+    
+    if(info && info.colorInfo && info.colorInfo == "-water-main-main-"){
+       h = 210; s = 50; l = 90;
+    }
+    
+    return(["hsla", h, s + "%", l + "%", a]);
+  
+  }else{
+    
+    const divNum = 1;
+    
+    let r = Math.floor(arr[1]/divNum)*divNum;
+    let g = Math.floor(arr[2]/divNum)*divNum;
+    let b = Math.floor(arr[3]/divNum)*divNum;
+    
+    return(["rgba", r, g, b, a]);
+  }
+  
+}
+
+/*************************************************/
+/*メイン                                         */
+/*************************************************/
+
+//テキスト形式を配列へ
+layers.forEach( layer => {
+  if(layer.visibility && layer.visibility == "none") return;
+  if(layer.paint){
+    for( name in layer.paint){
+      if(name.match("color")){
+        const colorInfo = layer.paint[name];
+        collectColorInfo(colorInfo); // 分析
+        layer.paint[name] = convertColor(colorInfo, [], { ...layer, "prop-name": name});
+        //console.log(layer.paint[name]);
+      }
+    }
+  }
+});
+
+
+console.log(tmp);
+//console.log(tmp2);
+//console.log(layers);
+
+
+const resstring = JSON.stringify(style);
+fs.writeFileSync("./docs/style.json", resstring);
+
---
+++ makeTemplate.js
@@ -0,0 +1,440 @@
+const fs = require('fs');
+
+const style = require('./std.json');
+
+const layers = style.layers;
+
+const tmp = {
+  "r": {}, "g": {}, "b": {}, "m": {}
+};
+const tmp2 = {};
+const tmp3 = {};
+const tmp4 = {};
+
+const collectColorInfo = (colorInfo, info={})=>{
+  if(Array.isArray(colorInfo)){
+    colorInfo.forEach( colorInfoElement => {
+      collectColorInfo(colorInfoElement);
+    });
+  }else if(typeof(colorInfo)=="string" && ( colorInfo.match(/^rgb/) || colorInfo.match(/^hsl/))){
+    
+    const ct = classifyColor(colorInfo);
+    
+    const identifier = (info.metadata) ? info.metadata.path || info.id : info.id;
+    
+    if(tmp[ct][colorInfo]){
+      tmp[ct][colorInfo] += 1;
+    }else{
+      tmp[ct][colorInfo] = 1;
+    }
+    return colorInfo;
+  }
+}
+
+const classifyColor = (colorInfo) => {
+  //colorInfoはString
+
+  const colStr = parseColor(colorInfo);
+  const cr = colStr[1];
+  const cg = colStr[2];
+  const cb = colStr[3];
+  
+  const ct = (cb == cr && cb == cg) ? "m" : (cr >= cg && cr >= cb) ? "r" : (cg >= cr && cg >= cb) ? "g" : "b";
+  
+  return ct;
+}
+
+/*************************************************/
+/*"rgba(r,g,b,a)"などのパース                    */
+/*************************************************/
+const parseColor = function(txt){
+  let length;
+  let type;
+  let col;
+  
+  if( (txt.indexOf("rgba") == 0) || (txt.indexOf("hsla") == 0) ){
+    length = txt.length - 1;
+    type= txt.slice(0, 4);
+    txt = txt.slice(5,length );
+    col = txt.split(",");
+  }else{
+    length = txt.length - 1;
+    type= txt.slice(0, 3) + "a";
+    txt = txt.slice(4,length );
+    col = txt.split(",");
+    col.push(1);
+  }
+  const color = [];
+  color.push( type );
+  color.push( parseInt(col[0]) );
+  color.push( parseInt(col[1]) );
+  color.push( parseInt(col[2]) );
+  color.push( Number(col[3]) );
+  return color;
+}
+
+const addArrEle = (arr, ele) => {
+  const n = arr.length;
+  if(Array.isArray(arr[n-1])){
+    addArrEle(arr[n-1],ele);
+  }else{
+    arr.push(ele);
+  }
+}
+
+const convertColor = (colorInfo, arr=[], info={}) => {
+  if(Array.isArray(colorInfo)){
+    colorInfo.forEach( colorInfoElement => {
+      arr.push(convertColor(colorInfoElement, [], info));
+    });
+    return arr;
+  }else if(typeof(colorInfo)=="string" && ( colorInfo.match(/^rgb/) || colorInfo.match(/^hsl/))){
+    
+    //修正
+    let colorInfo2
+    const carr = parseColor(colorInfo);
+    const colorKey = "rgb(${carr[1]},${carr[2]},${carr[3]})";
+    const opacity = carr[4];
+  
+    if(info["prop-name"].match("text-halo") ){
+      //縁取りの白は区別
+      colorInfo2 = "-text-white-halo-" + "#" + opacity;
+    }else if(info["prop-name"].match("text")){
+      colorInfo2 = setupColor(colorInfo, "text");
+      
+    }else if(info["source-layer"] == "RdCL"){
+    
+      if(colorInfo.match(/\(255,255,255/)){ 
+        colorInfo2 = "-road-normal-main-" + "#" + opacity;
+      }else if(colorInfo.match(/\(173,173,173/)){
+        colorInfo2 = "-road-edge-" + "#" + opacity;
+      }else if(colorInfo.match(/\(100,100,100/)){
+        colorInfo2 = "-road-edge-" + "#" + opacity;
+      }else if(colorInfo.match(/\(200,200,200/)){
+        colorInfo2 = "-road-edge-" + "#" + opacity;
+      }else if(colorInfo.match(/\(0,0,0/)){     
+        colorInfo2 = "-road-edge-" + "#" + opacity;
+      }else{
+        colorInfo2 = setupColor(colorInfo);
+      }
+      
+    }else if(info["source-layer"] == "RailCL"){
+    
+      if(colorInfo.match(/\(255,255,255/) && info.id.match("鉄道中心線旗竿")){
+        colorInfo2 = "-railway-normal-blank-" + "#" + opacity;
+      }else if(colorInfo.match(/\(255,255,255/)){
+        //基本的に「駅部分」の白を想定
+        colorInfo2 = "-railway-normal-main-" + "#" + opacity;
+      }else if(colorInfo.match(/\(173,173,173/)){
+        colorInfo2 = "-railway-normal-main-" + "#" + opacity;
+      }else if(colorInfo.match(/\(100,100,100/)){
+        colorInfo2 = "-railway-normal-main-" + "#" + opacity;
+      }else if(colorInfo.match(/\(0,0,0/)){
+        colorInfo2 = "-railway-normal-main-" + "#" + opacity;
+      }else{
+        colorInfo2 = setupColor(colorInfo);
+      }
+    }else{ 
+      colorInfo2 = setupColor(colorInfo);
+    }
+    
+    if(tmp3[colorInfo2]){
+      tmp3[colorInfo2] += 1;
+    }else{
+      tmp3[colorInfo2] = 1;
+    }
+    
+    if(tmp4[colorInfo]){
+      tmp4[colorInfo] += 1;
+    }else{
+      tmp4[colorInfo] = 1;
+    }
+    
+    console.log(colorInfo2);
+    
+    const identifier = (info.metadata) ? info.metadata.path || info.id : info.id;
+    if( colorInfo2.match(/^rgb/) || colorInfo2.match(/^hsl/) ){
+      if(tmp2[colorInfo2]){
+        tmp2[colorInfo2].push( identifier );
+      }else{
+        tmp2[colorInfo2] = [ identifier ];
+      }
+    }
+    
+    return(colorInfo2); 
+    
+  }else if(typeof(colorInfo)=="string" && colorInfo.match(/^-/)){
+    return(colorInfo);
+  }else{
+    return(colorInfo);
+  }
+  
+}
+
+
+const textColorSetOrg = {
+  
+  'rgb(0,0,255)': '-water-main-vivid-',
+  'rgb(60,50,181)': '-water-main-vivid-',
+  
+  'rgb(20,90,255)': '-water-main-vivid-', //水涯線
+  'rgb(190,210,255)': '-water-main-main-', //水域
+  'rgb(200,160,60)': '-landform-main-main-', //等高線
+  
+  'rgb(19,97,69)': '-text-green-main-', //〇文字色 e.g. station
+  
+  'rgb(160,160,160)': '-text-gray-main-', //〇文字色
+  'rgb(80,80,80)': '-text-gray-main-', //〇文字色
+  
+  'rgb(0,0,0)': '-text-black-main-', //〇文字色
+  
+  'rgb(255,255,255)': '-text-white-main-', //〇文字色
+  
+}
+
+
+const colorSetOrg = {
+
+  'rgb(255,255,255)': '-background-base-main-', 
+  
+  'rgb(255,230,190)': '-building-normal-main-', //●建物-普通
+  'rgb(255,187,153)': '-building-middle-main-', //●建物-堅ろう
+  'rgb(255,119,51)': '-building-high-main-', //●建物-高層
+  'rgb(255,135,75)': '-building-normal-outline-', //●建物
+  'rgb(255,135,75)': '-building-normal-outline-', //●建物
+
+  'rgb(100,195,115)': '-road-expressway-main-', //☆★高速道路
+  'rgb(235,130,120)': '-road-highway-main-', //☆★国道
+  'rgb(255,255,0)': '-road-prefectural-main-', //☆★県道
+  
+  //'rgb(0,0,255)': '-railway-shinkansen-main-', //鉄道-通常-新幹線
+  'rgb(100,0,0)': '-railway-normal-main-', //ZL17 軌道
+  'rgb(0,155,191)': '-railway-subway-main-', //★鉄道-地下鉄
+  
+  'rgb(190,210,255)': '-water-main-main-', //★水域
+  'rgb(20,90,255)': '-water-main-vivid-', //水涯線
+  'rgb(0,176,236)': '-water-main-vivid-', //水涯線(小ZL海岸線)
+  
+  'rgb(200,160,60)': '-landform-main-main-', //砂礫地
+  'rgb(200,250,230)': '-wetland-main-main-', //湿地
+  'rgb(217,217,217)': '-water-main-vivid-', //万年雪
+  'rgb(235,242,235)': '-landform-main-main-', //砂礫地
+  
+  'rgb(68,0,128)': '-border-muni-main-', //●市区町村界（紫）
+  'rgb(34,24,21)': '-border-muni-main-', //●行政区画界線国の所属界
+  'rgb(231,39,65)': '-line-gray-', //ZL6-8 航路-航路
+  'rgb(150,150,150)' : '-line-gray-', //特定地区界、その他の境界
+  
+  'rgb(100,100,100)' : '-line-gray-', //送電線
+  'rgb(200,200,200)': '-line-gray-', //ダム(面)
+
+  //100,100,100 200,200,200 の調査
+  //おそらく道路外周線、鉄道など
+  //0,0,0も同上か
+
+
+}
+
+
+const setupColor = (txt, type = "") => {
+  
+  const carr = parseColor(txt);
+  const colorKey = `rgb(${carr[1]},${carr[2]},${carr[3]})`;
+  const opacity = carr[4];
+  
+  const key = (type == "text") ? textColorSetOrg[colorKey] || "-text-gray-main-" : colorSetOrg[colorKey] || "--none-other--";
+  
+  if(!key.match("-none-")){
+    return key + "#" + opacity;
+  }else{
+    return txt;
+  }
+  
+}
+
+const changeColor = (arr, info={}) => {
+  
+  const mode = "_d";
+  
+  
+  const divNum = 1;
+  
+  let r = Math.floor(arr[1]/divNum)*divNum;
+  let g = Math.floor(arr[2]/divNum)*divNum;
+  let b = Math.floor(arr[3]/divNum)*divNum;
+  
+  return(["rgba", r, g, b, a]);
+  
+}
+/*************************************************/
+/*色以外の処理                                   */
+/*************************************************/
+const boundaryLayerConvert = (layer) => {
+  if(!layer["source-layer"] || layer["source-layer"] != "AdmBdry"){
+    return layer;
+  }
+  
+  if(layer.type == "line" && layer.id.match("行政区画界線地方界")){
+    layer.paint["line-dasharray"] = [ 10, 2, 1, 2 ];
+  }
+  
+  return layer;
+}
+
+const roadLayerConvert = (layer) => {
+  if(!layer["source-layer"] || layer["source-layer"] != "RdCL"){
+    return layer;
+  }
+  
+  if(layer.type == "line" && !layer.id.match("ククリ")){
+    if(!layer.layout) layer.layout = {};
+    layer.layout["line-cap"] = "round";
+  }
+  
+  /*
+  if(layer.id.match("道路中心線色1")){
+    layer.paint["line-dasharray"] = [4, 4];
+  }
+  */
+  
+  
+  return layer;
+}
+
+const railLayerConvert = (layer) => {
+  if(!layer["source-layer"] || layer["source-layer"] != "RailCL"){
+    return layer;
+  }
+  
+  if(layer.type == "line"){
+    if(!layer.layout) layer.layout = {};
+    layer.layout["line-cap"] = "round";
+  }
+  
+  if(layer.id.match("鉄道中心線旗竿")){
+    layer.layout.visibility = "none";
+    /*
+    //旗竿は、filter でJR線のみが対象となる
+    layer.minzoom = 11;
+    layer.paint["line-width"] = ["case",
+      [ "==", [ "get", "vt_sngldbl" ], "複線以上"], 2,
+      1
+    ];
+    */
+  }
+  
+  if(layer.id.match("鉄道中心線地下トンネル")){
+    //layer.paint["line-dasharray"] = [4, 4];
+    layer.paint["line-opacity"] = [ "case", 
+      [ "==", [ "get", "vt_sngldbl" ], "駅部分" ], 1, 
+      [ "case", 
+        [ "==", [ "get", "vt_railstate" ], "雪覆い" ], 0.75,
+        [ "==", [ "get", "vt_rtcode" ], "地下鉄" ], 0.5,
+      0.3 ] 
+    ];
+  }
+  
+  layer.paint["line-width"] = ["case",
+    [ "==", [ "get", "vt_sngldbl" ], "駅部分"], 3,
+    [ "==", [ "get", "vt_sngldbl" ], "複線以上"], 2,
+    1
+  ];
+  /*
+  layer.paint["line-gap-width"] = ["case",
+    [ "==", [ "get", "vt_sngldbl" ], "複線以上"], 1,
+    0
+  ];
+  */
+  
+  /*
+  layer.paint["line-width"] = ["case",
+    [ "==", [ "get", "vt_sngldbl" ], "駅部分"], 4,
+    [ "==", [ "get", "vt_sngldbl" ], "複線以上"], ["case",  ["==", [ "get", "vt_rtcode" ], "JR"], 3, 2],
+    ["==", [ "get", "vt_rtcode" ], "JR" ], 2,
+    1
+  ];
+  */
+  
+}
+
+const buildingLayerConvert = (layer) => {
+  
+  //順番を注記群の直前に持ってくる処理が必要
+  if(!layer["source-layer"] || (layer["source-layer"] != "BldA" && layer["source-layer"] != "StrctArea")){
+    return layer;
+  }
+  
+  if(layer.type != "fill"){
+    if(!layer.layout) layer.layout = {};
+    layer.layout.visibility = "none";
+    return layer;
+  }
+  
+  /*
+  //レイヤ順そのままでfill-extrusionとすると、ラインデータとの表示の重なりがおかしくなる。
+  layer.type = "fill-extrusion";
+  if(!layer.paint) layer.paint = {};
+  layer.paint["fill-extrusion-color"] = JSON.parse(JSON.stringify(layer.paint["fill-color"]));
+  layer.paint["fill-extrusion-height"] = ["match",
+    ["get", "vt_code"],
+    3103, 100,
+    3113, 100,
+    3102, 40,
+    3112, 40,
+    10
+  ];
+  layer.paint["fill-extrusion-opacity"] = 0.8;
+  
+  for(name in layer.paint){
+    if(!name.match(/extrusion/)) delete layer.paint[name];
+  }
+  
+  for(name in layer.layout){
+    if(!name.match(/extrusion/) && name != "visivility") delete layer.layout[name];
+  }
+  */
+  
+  return layer;
+  
+}
+
+
+/*************************************************/
+/*メイン                                         */
+/*************************************************/
+
+//テキスト形式を配列へ
+layers.forEach( layer => {
+  if(layer.paint){
+    for( name in layer.paint){
+      if(name.match("color")){
+        const colorInfo = layer.paint[name];
+        collectColorInfo(colorInfo, { ...layer, "prop-name": name}); // 分析
+        layer.paint[name] = convertColor(colorInfo, [], { ...layer, "prop-name": name});
+        //console.log(layer.paint[name]);
+      }
+    
+    }
+  }
+  
+  layer = boundaryLayerConvert(layer);
+  layer = roadLayerConvert(layer);
+  //layer = buildingLayerConvert(layer);
+  layer = railLayerConvert(layer);
+  
+});
+
+
+//console.log(tmp); //色ごとのレイヤ数（重複あり）
+//console.log(tmp2); //色ごとのレイヤリスト（重複あり）
+/*console.log(layers);*/
+
+//console.log(Object.keys(tmp3).sort()); //色リスト（変換後）
+//console.log(Object.keys(tmp4).sort()); //色リスト（変換前）
+
+console.log(tmp3);
+
+
+const resstring = JSON.stringify(style, null, 4);
+fs.writeFileSync("template.json", resstring);
+
